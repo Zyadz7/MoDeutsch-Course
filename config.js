d5cc149dd5651cb890a0de08213e6f3414f68770
Zyadz7 <mozizo2406@gmail.com>
2026-09-26 02:49:57 +0300

    Fix booking URL in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const sprachcafeConfig = {
     status: "open",
 
-    bookingUrl: "https://forms.google.com/https://appt.apptrainings.com/r/eFwkYmzxW6cBrvUi",
+    bookingUrl: "https://appt.apptrainings.com/r/eFwkYmzxW6cBrvUi",
 
     closedImage: "images/booking-closed.png",
 
